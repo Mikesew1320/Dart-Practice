42f8e16b976164138dc25f94306e23637f5952d0
Mikesew1320 <mikesew1320@gmail.com>
2025-11-25 12:07:57 +0600

    New push from Git bash @ TUE 25/11/2025 09:07AM

---
+++ main_2.dart
@@ -0,0 +1,344 @@
+import 'dart:ffi';
+
+void main_2() {
+  print("Hello World!");
+
+  // Different ways of declaring String Literals
+  String exp = 'This is string with single quotes';
+  String exp2 = "This is string with double quotes";
+  // In dart \ is an escape caracter in case of syntax confusion
+  String name = 'I\'am Googles NanoBannana';
+  print(exp);
+  print(exp2);
+  print(name);
+
+  String varDas =
+      'My name is '
+      'Michael '
+      'Bekele';
+  print(varDas);
+
+  // String Interpolation
+  String firstName = 'Michael';
+  String lastName = 'Bekele';
+  String desc = 'My name is ' + firstName;
+  String desc1 = 'My name is $lastName';
+  print(desc);
+  print(desc1);
+  print('My name is $firstName');
+  print('The length of the string desc is ' + desc.length.toString());
+  print('The length of the String desc1 is ${desc1.length}');
+
+  // Integer Interpolation
+
+  var a = 18;
+  var b = 34;
+  print("The sum of $a and $b is ${a + b} ");
+
+  // const and final keywords with and without datatype
+
+  const int num = 208;
+  const num1 = 657;
+
+  final double cat = 25.12;
+  final cat1 = 52;
+
+  print(num);
+  print(num1);
+  print(cat);
+  print(cat1);
+
+  // Conditional Statements
+  // if Statement
+  var nuk = 8;
+  if (nuk > 0) {
+    print("The number is Positive");
+  }
+
+  // if else Statement
+
+  var gvb = -7;
+  if (gvb > 0) {
+    print("The number is Positive");
+  } else {
+    print("The number is Negative");
+  }
+
+  // if else if ladder
+
+  String signal = "gr";
+  if (signal == "red") {
+    print("Stop please!");
+  } else if (signal == "yellow") {
+    print("Please wait...");
+  } else if (signal == "green") {
+    print("Go");
+  } else {
+    print("Not working!");
+  }
+
+  //Conditional expression
+
+  10 > 20 ? print("10 is greater") : print("10 is smaller"); //or
+
+  var xer = 30 > 20 ? "30 is greater" : "30 is smaller";
+  print(xer);
+
+  // Null checking expression
+  var xc = 56;
+  var cx = null;
+  var vcz = xc ?? cx;
+  print(vcz);
+
+  // switch statement
+
+  var grade = "F";
+  switch (grade) {
+    case "A":
+      {
+        print("Excellent");
+      }
+      break;
+    case "B":
+      {
+        print("Very Good");
+      }
+      break;
+    case "C":
+      {
+        print("Fair");
+      }
+      break;
+    case "D":
+      {
+        print("Poor");
+      }
+      break;
+    default:
+      {
+        print("Unknown Grade!");
+      }
+  }
+  // For loop
+  for (int a = 0; a <= 10; a += 2) {
+    print(a);
+  }
+  // For in loop
+  var fruits = [
+    "Apple",
+    "Mango",
+    "Banana",
+    "Cherry",
+    "Grapes",
+    "Pineapple",
+    "Orange",
+  ];
+  for (var vaxi in fruits) {
+    print(vaxi);
+  }
+  // While loop
+  var table = 20;
+  var kind = 20;
+  while (table >= 0) {
+    print("$kind x $table=${kind * table}");
+    table--;
+  }
+  // Do while loop
+  /*var inc = 10;
+  do {
+    print(inc);
+    inc--;
+  } while (inc >= 0);*/
+
+  // Break statement
+  /*var kat = 1;
+  while (kat <= 30) {
+    print(kat);
+    if (kat == 20) {
+      break;
+    }
+    kat++;
+  }*/
+
+  // Continue Statement
+  //var ijk = 1;
+  /*for (var ijk = 1; ijk <= 20; ijk++) {
+    
+    if (ijk == 10) {
+      continue;
+      }
+      print(ijk);
+    }
+  }*/
+
+  // Labels
+  outerLoop:
+  for (int i = 1; i <= 5; i++) {
+    if (i == 3) {
+      continue outerLoop;
+    }
+    innerLoop:
+    for (int j = 1; j <= 5; j++) {
+      if (i == 3 && j == 1) {
+        continue innerLoop;
+      }
+      print("$i $j");
+    }
+
+    dream_func();
+    some_func(22, "Michael");
+    erruda_os(28, 23.567);
+    //some_func();
+    city_names("Jimma", "Addis Abeba");
+    franklin("Masor", asx: "Damat", cxz: "Uskudar");
+    carPlates(145263, fgh: 785458);
+  }
+  // Exception Handling
+
+  double exept = 12 / 0;
+  print(exept);
+
+  // Creating Exeption with divition by 0
+
+  /*int frozen = 12 ~/ 0;
+      print(frozen);*/
+
+  // Handling the above exeption
+  try {
+    int frozen = 12 ~/ 0;
+    print(frozen);
+  } on IntegerDivisionByZeroException {
+    print("Can not divide by zero");
+  }
+  // Catch Block
+  try {
+    int secto = 34 ~/ 0;
+    print(secto);
+  } catch (e) {
+    print("The exception thrown is handled by $e");
+  }
+
+  // Exception with stack trace
+
+  /*try {
+    int dexopt = 52 ~/ 0;
+    print(dexopt);
+  } catch (e, s) {
+    print("The exception thrown this time is handled by $e");
+    print("The stack trace is $s");
+  }*/
+
+  // Finally clause
+
+  try {
+    int dexty = 60 ~/ 12;
+    print(dexty);
+  } catch (e, s) {
+    print("The exception thrown is handled by $e");
+    print("The stack trace is $s");
+  } finally {
+    print("This is finally clause and it will excute always");
+  }
+
+  try {
+    checkNumber(-30);
+  } catch (e) {
+    var msg = checkNegativeException();
+    print(msg.errMsg());
+  }
+
+  var std1 = Student();
+
+  std1.id = 3453;
+  std1.name = "Ephrem";
+
+  print("The ID of the Student is ${std1.id}");
+  print("The Name of the Student is ${std1.name}");
+
+  std1.study();
+  std1.eat();
+  std1.sleep();
+
+  var std2 = Student();
+
+  std2.id = 5674;
+  std2.name = "Firaol";
+
+  print("The ID of the student is ${std2.id}");
+  print("The Name of the Student is ${std2.name}");
+
+  std2.study();
+  std2.eat();
+  std2.sleep();
+}
+
+// Custom exception handling class
+
+void checkNumber(int numberw) {
+  if (numberw < 0) {
+    throw checkNegativeException();
+  }
+}
+
+class checkNegativeException implements Exception {
+  String errMsg() {
+    return "The value can\'t be negative number";
+  }
+}
+
+//Functions
+String dream_func() {
+  //print("Being an Android Developer");
+  return "Function Called";
+}
+
+//Functions with Parameter
+
+some_func(int xg, String name) {
+  print(xg);
+  print(name);
+}
+
+// Arrow or lambda Functions
+erruda_os(int rex, double vcx) => print("$rex, $vcx");
+
+// Optional Positional Parameter
+city_names(String city1, String city2, [String? city3]) {
+  print("The name of city 1 is $city1");
+  print("The name of city 2 is $city2");
+  print("The name of city 3 is $city3");
+}
+
+// Optional Named Parameter
+
+franklin(String adb, {String? asx, String? cxz}) {
+  print("The root name is $adb");
+  print("The home name is $asx");
+  print("The binary name is $cxz");
+}
+
+// Optional Default Parameter
+
+carPlates(int asd, {int? fgh = 145275, int? dlk = 143585}) {
+  print("The first car plate number is $asd");
+  print("The second car plate number is $fgh");
+  print("The third car plate number is $dlk");
+}
+
+// Object oriented Programing in Dart refer above at line #250
+class Student {
+  var id;
+  var name;
+
+  void study() {
+    print("${this.name} is studying now");
+  }
+
+  void sleep() {
+    print("${this.name} is sleeping at 12:50AM");
+  }
+
+  void eat() {
+    print("${this.name} is eating at 3:23PM");
+  }
+}
